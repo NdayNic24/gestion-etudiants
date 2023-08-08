1894e097940431229f421653ee820611b297cce8
unknown <nicolasndayizeye24@gmail.com>
2023-08-07 18:03:15 -1100

    added email and password

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+
+?>
+<html>
+<head>
+<title>page d'authantification</title>
+
+</head>
+<body>
+<form method="post" action="savelogin.php">
+<label>username</label>
+<input type="text" name="username"/><br></br>
+<label>password</label>
+<input type="text" name="password"/></br><br>
+<input type="submit" value="se connecter">
+
+</form>
+</body>
+
+
+</html>
